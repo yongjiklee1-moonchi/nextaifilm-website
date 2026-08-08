26fd1ab862022d943f8b323a86294bdf3240a4cc
yongjiklee1-moonchi <yongjiklee1@gmail.com>
2026-08-08 20:52:55 +0900

    Add files via upload

--- js/cms-config.js
+++ js/cms-config.js
@@ -10,7 +10,7 @@
  * 비어 있으면 CMS는 동작하지 않고, 페이지에 적힌 기존 HTML 문구가 그대로 보입니다.
  */
 window.NAF_CMS_CONFIG = {
-  WEB_APP_URL: "https://script.google.com/macros/s/AKfycbwd2DAnIR8r7FIcDXJdQEJ5kzMQeY45eO71xWt55XcH8crViuD6ecySFVRPrt5EtvzK/exec",
+  WEB_APP_URL: "https://script.google.com/macros/s/AKfycbzxflmPdifREqT7Ffrxg_KeofOTNsI_m3EPpuf3y2SvUgGOMQm8mGUvMh2YnDf-tLGdZw/exec",
   // 캐시 시간(분). 시트 수정 후 바로 보려면 1~5 권장
   CACHE_MINUTES: 5,
   // 디버그 로그
