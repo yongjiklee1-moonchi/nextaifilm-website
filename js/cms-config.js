14d33e1717e39dc1c48359bd8f2aa85a8fb783de
NAF <yongjiklee1@gmail.com>
2026-09-20 23:44:44 +0900

    Add files via upload

--- js/cms-config.js
+++ js/cms-config.js
@@ -1,18 +1,13 @@
 /**
- * NAF CMS config
- *
- * 1) Google 시트(NAF Website CMS)에서 Apps Script 웹 앱을 배포한 뒤
- * 2) 아래에 웹 앱 URL을 붙여넣으세요. (.../exec 로 끝나는 주소)
- *
- * 예:
- *   WEB_APP_URL: "https://script.google.com/macros/s/AKfycb.../exec"
- *
- * 비어 있으면 CMS는 동작하지 않고, 페이지에 적힌 기존 HTML 문구가 그대로 보입니다.
+ * Google Apps Script connection for NAF.
+ * AUTH_URL / WEB_APP_URL: screening login + inquire form.
+ * Page copy still comes from HTML (cms.js is not loaded on pages).
  */
 window.NAF_CMS_CONFIG = {
-  WEB_APP_URL: "https://script.google.com/macros/s/AKfycbzxflmPdifREqT7Ffrxg_KeofOTNsI_m3EPpuf3y2SvUgGOMQm8mGUvMh2YnDf-tLGdZw/exec",
-  // 캐시 시간(분). 시트 수정 후 바로 보려면 1~5 권장
+  WEB_APP_URL:
+    "https://script.google.com/macros/s/AKfycbzxflmPdifREqT7Ffrxg_KeofOTNsI_m3EPpuf3y2SvUgGOMQm8mGUvMh2YnDf-tLGdZw/exec",
+  AUTH_URL:
+    "https://script.google.com/macros/s/AKfycbzxflmPdifREqT7Ffrxg_KeofOTNsI_m3EPpuf3y2SvUgGOMQm8mGUvMh2YnDf-tLGdZw/exec",
   CACHE_MINUTES: 5,
-  // 디버그 로그
   DEBUG: false
 };
