9247623b277bde5352a65b3a17055f2136121f0f
NAF <yongjiklee1@gmail.com>
2026-09-20 23:34:18 +0900

    Add files via upload

--- js/sunflowers-config.js
+++ js/sunflowers-config.js
@@ -4,7 +4,7 @@
  */
 window.NAF_SUNFLOWERS = {
   user: "sunflowers",
-  password: "1234",
+  password: "7890",
   sessionHours: 1,
   embedUrl:
     "https://player.vimeo.com/video/1212021254?badge=0&autopause=0&player_id=0&app_id=58479&title=0&byline=0&portrait=0"
